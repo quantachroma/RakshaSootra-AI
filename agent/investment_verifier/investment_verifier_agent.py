8fd3b3acafe13e4fd14bca5a63abff7178a75530
Ritisha Sharma <ritsharma386@gmail.com>
2026-07-16 13:20:57 +0530

    Investment file modifiied

--- agent/investment_verifier/investment_verifier_agent.py
+++ agent/investment_verifier/investment_verifier_agent.py
@@ -271,9 +271,9 @@ def run_investment_verifier(state):
     return {
         "investment_result": result
     }
-def run_investment_verifier(state: dict) -> dict:
-    """Person 3 will build the real logic here later."""
-    state["risk_level"] = "risky"
-    state["explanation"] = "[Investment Verifier] Platform not found in SEBI registry. Promises unrealistic returns."
-    state["extracted_entities"] = {"platform": ["CryptoMax Profit"]}
-    return state
+# def run_investment_verifier(state: dict) -> dict:
+#     """Person 3 will build the real logic here later."""
+#     state["risk_level"] = "risky"
+#     state["explanation"] = "[Investment Verifier] Platform not found in SEBI registry. Promises unrealistic returns."
+#     state["extracted_entities"] = {"platform": ["CryptoMax Profit"]}
+#     return state
